a65e9d8750f14593b5625964bd41d3c410cb26fa
DeehSlash <andreluiz.97@outlook.com>
2015-11-27 15:26:47 -0200

    Alterada precisão do cout e adicionado comentários

--- main.cpp
+++ main.cpp
@@ -16,19 +16,19 @@
 #include <stdlib.h> // para usar comandos como srand, rand.
 #include <time.h> // time auxilia no rand
 
-#include "dimensaojanela.h"
-#include "menu.h"
-#include "struct_ranking.h"
+#include "dimensaojanela.h" // para alterar a dimensão da janela do console
+#include "menu.h" // para poder chamar o menu
+#include "struct_ranking.h" // para poder criar o vetor de struct
 
 using namespace std;
 
-int main()
-{
+int main(){
     system("color f0"); // cor do console
-    DimensaoJanela(90, 30);
+    DimensaoJanela(90, 30); // altera a dimensão da janela do console
     setlocale(LC_ALL, "Portuguese_Brazil"); // para a acentuação funcionar
     srand(time(NULL)); // para usar random
+    cout.precision(3);
     int tam = 3, dificuldade = 1; // variáveis int
-    ranking ranking[10];
-    menu(tam, dificuldade, ranking); // chama o menu
+    ranking top[10]; // cria o vetor top 10 da struct ranking
+    menu(tam, dificuldade, top); // chama o menu
 }
